6fa64800174e11621d544385d77ef399407bdd8f
NhuY29 <100512487+NhuY29@users.noreply.github.com>
2024-02-03 14:30:00 +0700

    ...

---
+++ DoAnWeb/example-app/resources/views/home.blade.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dự Án Nuôi Em</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-...</style>" crossorigin="anonymous" />
+</head>
+
+<body>
+  <nav>
+    <a href="#">Giới Thiệu Chung</a>
+    <a href="#">Tài Chính</a>
+    <a href="#">Nhận Mã</a>
+    <a href="#">Tra Mã</a>
+    <a href="#">Thăm Em</a>
+    <a href="#">Số Tài Khoản</a>
+    <a href="#">Hệ Sinh Thái</a>
+  </nav>
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://xanhtueduc.edu.vn/thumbs/1500x650x1/upload/photo/slider-11780.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://i.ytimg.com/vi/SmxPzjIM83M/maxresdefault.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://dntt.mediacdn.vn/197608888129458176/2023/6/12/z4417382735986f6566aa77a9f6548012e6f4e908da373-16865331557961772883558.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade1" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <div class="Com">
+    <div class="GT">
+      <h1 style="font-weight: 900;">Sức Mạnh Bữa Ăn Trưa 8.500 Đồng <i class="fa-solid fa-fire"></i></h1>
+      <p>Dự án “Nuôi em” bắt đầu hoạt động từ năm 2014, do tổ chức thiện nguyện Niềm tin phụ trách.</p>
+      <p>Nhận thấy rằng, dù đã được hỗ trợ xây trường, cung cấp sách vở nhưng các em vùng cao vẫn phải bỏ học vì “đói ăn” khi đến trường. Đây là nguyên nhân trực tiếp khiến hơn 90% các em bỏ học.</p>
+      <p>Do vậy, dự án “Nuôi em” đã ra đời, với mục tiêu cung cấp những bữa ăn đầy đủ, giúp các em có thêm động lực đến trường. Mỗi bữa ăn có chi phí chỉ 8500 đồng do các thầy cô tại trường nấu, nhưng đầy đủ chất dinh dưỡng, ấm nóng tình thương, là một động lực vô cùng to lớn tiếp thêm sức mạnh cho các bé trên con đường đến trường.</p>
+    </div>
+    <div class="dagiac">
+      <img src="https://th.bing.com/th/id/OIP.sqRY7pYg1znr9Z4exIJLoQHaE9?rs=1&pid=ImgDetMain" id="hexagon-image" alt="...">
+      <img src="https://mytourcdn.com/upload_images/Image/Minh%20Hoang/Tay%20bac/tre%20em/3.jpg" id="hexagon-image1" alt="...">
+      <img src="https://images.baoquangnam.vn/Storage/NewsPortal/2021/9/17/117512/TNB-47496.jpg" id="hexagon-image2" alt="...">
+      <img src="https://img1.kienthucvui.vn/uploads/2021/02/11/anh-tre-em-ngheo-vung-cao-di-hoc_114006106.jpg" id="hexagon-image3" alt="...">
+      <img src="https://th.bing.com/th/id/OIP.vQjON6D-WRZb4BCMwPUwbgHaE7?rs=1&pid=ImgDetMain" id="hexagon-image4" alt="...">
+      <img src="https://www.worldatlas.com/twitter/satrip/boca/theline.jpg" id="hexagon-image5" alt="...">
+      <img src="https://th.bing.com/th/id/OIP.gXlxedd-AxF3AKpZbAz3AAHaE8?rs=1&pid=ImgDetMain" id="hexagon-image6" alt="...">
+    </div>
+  </div>
+  <div class="QuyTac">
+  <div class="QuyTacConAnh">
+      dksjfsdjfkdfjsdfsd
+    </div>
+    <div>
+      <h5><i class="fa-solid fa-key"></i>Mỗi Em Chỉ 01 Mã</h5>
+      <p>Mỗi em một mã và 01 ảnh duy nhất, không trùng trong hàng NGHÌN em nhỏ.</p>
+      <div>
+      <h5><i class="fa-solid fa-phone"></i>Mỗi Em Chỉ 01 Người Nhận Nuôi</h5>
+      <p>Để đảm bảo an toàn, bảo mật thông tin cho các bé, mỗi bé chỉ có 01 người nhận nuôi duy nhất.</p>
+    </div>
+    <div>
+      <h5><i class="fa-solid fa-user-plus"></i>Một Người Có Thể Nuôi Nhiều Bé</h5>
+      <p>Chúng tôi không giới hạn số lượng bé trên mỗi người nuôi.</p>
+    </div>
+    <div>
+      <h5><i class="fa-solid fa-gift"></i>Khuyến Khích Nuôi Bé Nhiều Hơn 1 Năm</h5>
+      <p>Mỗi bé đều đặn đi học, vì thế chúng tôi khuyến khích bạn tham gia nuôi bé nhiều hơn 1 năm.</p>
+    </div>
+    </div>  
+  </div>
+
+</body>
+
+</html>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+  }
+
+  header {
+    background-color: #333;
+    color: white;
+    padding: 10px;
+    text-align: center;
+  }
+
+  nav {
+    display: flex;
+    justify-content: center;
+    background-color: #ddd;
+    padding: 10px;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: #333;
+    padding: 10px 20px;
+    margin: 0 10px;
+    border-radius: 5px;
+    background-color: #fff;
+    transition: background-color 0.3s;
+  }
+
+  nav a:hover {
+    background-color: #333;
+    color: white;
+  }
+
+  #carouselExampleFade {
+    width: 100%;
+    height: 500px;
+  }
+
+  .carousel-item>img {
+    width: 100%;
+    height: 500px;
+  }
+
+  .Com {
+    width: 100%;
+    height: 550px;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bold;
+    margin-top: 20px
+  }
+
+  #hexagon-image {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: -16.5px;
+    margin-left: 9px;
+  }
+
+  #hexagon-image1 {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: -16.5px;
+    margin-left: 209px;
+  }
+
+  #hexagon-image2 {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: 133.5px;
+    margin-left: 110px;
+  }
+
+  #hexagon-image3 {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: 133.5px;
+    margin-left: 309px;
+  }
+
+  #hexagon-image4 {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: 284.5px;
+    margin-left: 210px;
+  }
+
+  #hexagon-image5 {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: 133.5px;
+    margin-left: -91px;
+  }
+
+  #hexagon-image6 {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    margin-top: 283.5px;
+    margin-left: 9px;
+  }
+
+  .Com {
+    overflow: hidden;
+    /* Clear float để giữ cho container bao bọc cả nội dung và ảnh */
+    padding: 2%
+  }
+
+  .GT {
+    float: left;
+    width: 60%;
+  }
+
+  .GT>p {
+    width: 80%;
+  }
+
+  .DG {
+    float: left;
+    width: 40%;
+  }
+
+  .QuyTac {
+    width: 100%;
+    height: 500px;
+    display: grid;
+    grid-template-columns:60% 40%;
+  }
+</style>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
