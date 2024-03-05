54c6a6e1b6d3cb85edb0e3b10551ea9535cb6884
NhuY29 <100512487+NhuY29@users.noreply.github.com>
2024-03-05 09:26:36 +0700

    ...

--- DoAnWeb/example-app/resources/views/TreEm.blade.php
+++ DoAnWeb/example-app/resources/views/TreEm.blade.php
@@ -82,12 +82,97 @@
                 </tr>
             </thead>
             <tbody>
+            @foreach($allTreEmRecords as $index => $record)
+                <tr id="row{{ $record->id }}" data-id="{{ $record->id }}" class="{{ $index % 2 === 0 ? 'bg-white' : 'bg-blue-100' }} border">
+                    <td class="px-4 py-2 text-center border whitespace-normal max-w-xs">{{ ++$i }}</td>
+                    <td class="px-4 py-2 text-center border whitespace-normal max-w-xs">{{ $record->Ten }}</td>
+                    <td class="px-4 py-2 text-center border whitespace-normal max-w-xs">{{ $record->GioiTinh ? 'Nam' : 'Nữ' }}</td>
+                    <td class="px-4 py-2 text-center border whitespace-normal max-w-xs">{{ $record->TenTruongHoc }}</td>
+                    <td class="px-4 py-2 text-center border whitespace-normal max-w-xs">{{ $record->DiaChi }}</td>
+                    <td class="px-4 py-2 text-center border whitespace-normal max-w-xs">{{ $record->BenThu3_id }}</td>
+                    <td class="px-4 py-2 text-center border whitespace-nowrap max-w-xs">
+                        <form id="deleteForm{{ $record->id }}" action="/delete/{{ $record->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" data-id="{{ $record->id }}" onclick="deleteRow(this)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Xóa</button>
+                            <!-- <button type="button" onclick="editForm({{ $record->id }})" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Chỉnh Sửa</button> -->
+                            <button type="button" onclick="toggleEditForm({{ $record->id }})" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Chỉnh Sửa</button>
+                        </form>
+                    </td>
+                </tr>
+                <div id="editForm{{ $record->id }}" style="display: none;" class="max-w-md mx-auto my-14">
+                    <form action="/update/{{ $record->id }}" method="post" style="display: none;" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-4">
 
+                            <label for="Ten_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Tên:</label>
+
+                            <input type="text" id="Ten_edit{{ $record->id }}" name="Ten" value="{{ $record->Ten }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+
+                        </div>
+                        <div class="mb-4">
+
+                            <label for="GioiTinh_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Giới tính:</label>
+
+                            <select id="GioiTinh_edit{{ $record->id }}" name="GioiTinh" class="block appearance-none w-full bg-white border border-gray-400 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:border-blue-500">
+
+                                <option value="1" {{ $record->GioiTinh == 1 ? 'selected' : '' }}>Nam</option>
+
+                                <option value="0" {{ $record->GioiTinh == 0 ? 'selected' : '' }}>Nữ</option>
+
+                            </select>
+
+                        </div>
+                        <div class="mb-4">
+
+                            <label for="TenTruongHoc_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Tên Trường Học:</label>
+
+                            <textarea id="TenTruongHoc_edit{{ $record->id }}" name="TenTruongHoc" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $record->DiaChi }}</textarea>
+
+                        </div>
+                        <div class="mb-4">
+
+                            <label for="DiaChi_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Địa chỉ:</label>
+
+                            <textarea id="DiaChi_edit{{ $record->id }}" name="DiaChi" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $record->DiaChi }}</textarea>
+
+                        </div>
+
+
+                        <div class="mb-4">
+
+                            <label for="BenThu3_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Bên thứ 3:</label>
+
+                            <input type="text" id="BenThu3_edit{{ $record->id }}" name="BenThu3_id" value="{{ $record->NoiCongTac }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+
+                        </div>
+                        <div class="flex items-center justify-between">
+
+                            <button type="button" onclick="cancelEdit({{ $record->id }})" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Hủy</button>
+
+                            <input type="submit" value="Xác Nhận" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+
+                        </div>
+                    </form>
+                </div>
+                @endforeach
+                <div class="flex items-center my-4">
+                    <button onclick="toggleAddForm()" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Thêm</button>
+                    <div class="flex items-center ml-4">
+
+                        <form action="{{ route('search') }}" method="POST">
+                            @csrf
+                            <input name="search-items-name" type="text" class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" placeholder="Tìm Kiếm">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg ml-2">Search</button>
+                        </form>
+                    </div>
+                </div>
             </tbody>
 
         </table>
 
-
+        {{ $allTreEmRecords->links() }}
 
     </div>
     </div>
