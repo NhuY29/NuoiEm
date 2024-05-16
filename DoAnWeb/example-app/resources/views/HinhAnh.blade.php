584aa3e0ab73abc44f8e73d3a0e59451adf4ee50
LeBao2808 <126840009+LeBao2808@users.noreply.github.com>
2024-05-16 20:12:08 +0700

    ggg

--- DoAnWeb/example-app/resources/views/HinhAnh.blade.php
+++ DoAnWeb/example-app/resources/views/HinhAnh.blade.php
@@ -117,7 +117,11 @@
 
                             <label for="DuongDan_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Đường Dẫn:</label>
 
+<<<<<<< HEAD
                             <input type="file" id="DuongDan_edit{{ $record->id }}" name="filess" value="{{ $record->DuongDan }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+=======
+                            <input type="file" id="DuongDan_edit{{ $record->id }}" name="DuongDan" value="{{ $record->DuongDan }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+>>>>>>> 94ffa936ba036362df6ed69da9c519d91b7b7031
 
                         </div>
 
