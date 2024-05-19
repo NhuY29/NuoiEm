925ff32f24f01e17477d166b4898c1f8a270318f
LeBao2808 <126840009+LeBao2808@users.noreply.github.com>
2024-05-19 20:24:32 +0700

    baivietupdate

--- DoAnWeb/example-app/resources/views/BaiViet.blade.php
+++ DoAnWeb/example-app/resources/views/BaiViet.blade.php
@@ -156,7 +156,7 @@
                         <div class="mb-4">
                             <label for="user_id_edit{{ $record->id }}" class="block text-gray-700 text-sm font-bold mb-2">Người dùng:</label>
                             <div class="relative">
-                                <select id="user_id_edit{{ $record->id }}" name="TreEm_id" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                                <select id="user_id_edit{{ $record->id }}" name="user_id" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                                     @foreach ($allUerRecords as $URecordRecord)
                                     <option value="{{ $URecordRecord->id }}" {{ $URecordRecord->id == $record->user_id ? 'selected' : '' }}>{{ $URecordRecord->name }}</option>
                                     @endforeach
